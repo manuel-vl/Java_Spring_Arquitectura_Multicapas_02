04def2c10d2db8c82147df035e2a5b05ad9f2038
manuel-vl <manuel.valencia@mercadolibre.com.co>
2024-02-09 15:01:56 -0500

    Initial commit

---
+++ src/main/java/com/calculadoracalorias/apicaloriasalimentos/ApiCaloriasAlimentosApplication.java
@@ -0,0 +1,13 @@
+package com.calculadoracalorias.apicaloriasalimentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCaloriasAlimentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCaloriasAlimentosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/calculadoracalorias/apicaloriasalimentos/ApiCaloriasAlimentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calculadoracalorias.apicaloriasalimentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCaloriasAlimentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
